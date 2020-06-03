411de4e4804930266cce61385b06e243859765ea
J.guzman <61435395+jguzman11@users.noreply.github.com>
2020-06-02 21:46:04 -0400

    react-project

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+function Footer() {
+
+    return (
+        <div className="main-footer">
+            <div className="social">
+              <span className="icon"><a href="https://twitter.com/"><i class="fa fa-twitter fa-lg" aria-hidden="true"></i></a></span>
+              <span className="icon"><a href="https://www.facebook.com/"><i class="fa fa-facebook fa-lg" aria-hidden="true"></i></a></span>
+              <span className="icon"><a href="https://www.instagram.com/"><i class="fa fa-instagram fa-lg" aria-hidden="true"></i></a></span>
+            </div>
+            <p>7O4 Kicks &copy; 2020, All Rights Reserved</p>
+          </div>
+    );
+}
+export default Footer;
+
+
+        // <footer id="main-footer">
+        //     <div class="social">
+        //         <span class="icon"><a href="https://twitter.com/"><i class="fa fa-twitter fa-lg" aria-hidden="true"></i></a></span>
+        //         <span class="icon"><a href="https://www.facebook.com/"><i class="fa fa-facebook fa-lg" aria-hidden="true"></i></a></span>
+        //         <span class="icon"><a href="https://www.instagram.com/"><i class="fa fa-instagram fa-lg" aria-hidden="true"></i></a></span>
+        //     </div>
+        //     <p>7o4 Kicks &copy; 2020, All Rights Reserved</p>
+        // </footer>
